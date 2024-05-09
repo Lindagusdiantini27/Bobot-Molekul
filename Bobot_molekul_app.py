6dbb4ce2c07090370e27ba9dca896512a9f802cc
Lindagusdiantini27 <165452746+Lindagusdiantini27@users.noreply.github.com>
2024-05-09 14:05:44 +0700

    Add files via upload

--- Bobot_molekul_app.py
+++ Bobot_molekul_app.py
@@ -146,7 +146,6 @@ else:
 # Menampilkan skor akhir
         st.write(f"skor akhir Anda adalah {score} dari {len(questions)} soal.")
 st.markdown
-        position: fixed; 
         left: 0;
         bottom: 0;
         width: 100%;
